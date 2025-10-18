a8b098c2ce6a3dd9420f47274bdf97d37abf4ad3
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2025-10-18 21:54:24 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (56.98%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -1,29 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void helper(TreeNode*root,int &ans,bool isflag){
-        if(!root) return ;
-       
-        helper(root->right,ans,false);
-    
-        helper(root->left,ans,true);
-        if(!root->left&&!root->right&&isflag) ans+=root->val;
+    void helper(TreeNode* root, int& ans, int flag) {
+        if (!root) return;
+        if (!root->left && !root->right && flag == 1) {
+            ans += root->val;
+        }
+        helper(root->left, ans, 1);  
+        helper(root->right, ans, 0); 
     }
+
     int sumOfLeftLeaves(TreeNode* root) {
-        int ans=0;
-        bool isflag=0;
-         if(!root->left&&!root->right) return 0;
-        helper(root,ans,isflag);
+        int ans = 0;
+        // use a variable flag so when we at left it is 1 and when we at right it is 0 
+       
+        helper(root, ans, 0);
         return ans;
     }
 };
