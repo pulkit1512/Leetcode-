512e2691321da2e9217fefeaa3a1a8b1b7fffdba
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2025-10-18 14:16:24 +0530

    Time: 4 ms (45.67%) | Memory: 8.9 MB (44.73%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -4,11 +4,11 @@ public:
         int x=n;
         if(n<0||n==0) return 0;
         long long ans=1;
-        while(n){
+        while(ans<=n){
             if(ans==x) return 1;
             ans=ans*3;
             
-            n/=3;
+            
         }
         return 0;
     }
