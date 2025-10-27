5ccdfd95f4769f74aa9a31c52676bdb00c7bc35e
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2025-10-27 16:09:46 +0530

    Time: 80 ms (20.49%) | Memory: 75.8 MB (36.31%) - LeetSync

--- 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.cpp
+++ 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.cpp
@@ -9,7 +9,7 @@ class Solution {
 public:
     string kthLargestNumber(vector<string>& nums, int k) {
         string ans="";
-        
+        // we cannot directly put the string in the pq because it sort it lexiographically not by integer in it "10"<"2" so use the different cmp same size string is cmp lexiographically and the different length on the basis of length of string 
         priority_queue<string,vector<string>,cmp>pq;
         for(int i=0;i<nums.size();i++){
             pq.push(nums[i]);
