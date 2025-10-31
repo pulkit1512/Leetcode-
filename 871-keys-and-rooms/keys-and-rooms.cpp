fe2165c1fa4b734c95f128bbcdf2a940c95cb489
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2025-10-31 15:43:55 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (32.60%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
+    void dfs(int node,vector<vector<int>>&rooms,vector<bool>&visited,vector<int>&ans){
+        visited[node]=1;
+        ans.push_back(node);
+        for(int i=0;i<rooms[node].size();i++){
+            int neighbour =rooms[node][i];
+            if(!visited[neighbour]){
+                dfs(neighbour,rooms,visited,ans);
+            }
+        }
+    }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int v=rooms.size();
         vector<bool>visited(v,0);
-        queue<int>q;
-        q.push(0);
-        visited[0]=1;
         vector<int>ans;
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            ans.push_back(node);
-            for(int i=0;i<rooms[node].size();i++){
-                int neighbour=rooms[node][i];
-                if(!visited[neighbour]){
-                    visited[neighbour]=1;
-                    q.push(neighbour);
-                }
-            }        }
-            int count=ans.size();
-            return count==v;
+        dfs(0,rooms,visited,ans);
+        int count=ans.size();
+        return count==v;
     }
 };
