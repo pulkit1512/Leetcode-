acee7c9366da17f4d2b305724040b5ba87d3442a
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2025-10-31 14:47:03 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (75.13%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        int v=graph.size();
-        vector<int>color(v,-1);
-        for(int i=0;i<v;i++){
-            if(color[i]==-1){
-                queue<int>q;
-                q.push(i);
-                color[i]=0;
-                while(!q.empty()){
-                    int node=q.front();
-                    q.pop();
-                    for(int j=0;j<graph[node].size();j++){
-                        //if color is not assigned
-                        int neighbour=graph[node][j];
-                        if(color[neighbour]==-1){
-                            color[neighbour]=(color[node]+1)%2;
-                             q.push(neighbour);
-                        }
-                        else{
-                            //check if assigned color is same as the neighbour and if same return 0                      
-                            if(color[node]==color[neighbour]){
-                                return 0;
-                            }
-                        }
-                    }
+    bool isbip(int node, vector<vector<int>> &adj, vector<int> &color) {
+        for (int neighbour : adj[node]) {
+            if (color[neighbour] == -1) {
+                color[neighbour] = 1 - color[node];
+                if (!isbip(neighbour, adj, color)) {
+                    return false;
+                }
+            } else if (color[neighbour] == color[node]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool isBipartite(vector<vector<int>> &adj) {
+        int V=adj.size();
+        vector<int> color(V, -1);
+        for (int i = 0; i < V; i++) {
+            if (color[i] == -1) {
+                color[i] = 0;
+                if (!isbip(i, adj, color)) {
+                    return false;
                 }
             }
         }
-        return 1;
+        return true;
     }
 };
+
