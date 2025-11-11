bd9626f946261bd6bcffb0b34ee55eaee4df00ff
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2025-11-11 14:22:49 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (6.60%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string helper(int n){
+        string s;
+        while(n){
+            int x=n%2;
+            s.push_back('0'+x);
+            n=n/2;
+        }
+        while(s.length()<32){
+            s.push_back('0');
+        }
+        reverse(s.begin(),s.end());
+        return s;
+    }
+    int hammingDistance(int x, int y) {
+        string str1 =helper(x);
+        string str2=helper(y);
+        int i=0,j=0;
+        int ans=0;
+        while(i<32){
+            if(str1[i]!=str2[j]){
+                ans++;
+            }
+            i++,j++;
+        }
+        return ans;
+    }
+};
