3c33185f2711a805c5c8a2819bdf50ac9f2bb937
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2025-11-20 19:46:40 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (73.58%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    int find(int stair,int n,vector<int>&cost,vector<int>&dp){
-        if(stair>=n) return 0;
-        if(dp[stair]!=-1) return dp[stair];
-        dp[stair]=cost[stair]+min(find(stair+1,n,cost,dp),find(stair+2,n,cost,dp));
-        return dp[stair];
-    }
+    
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
-        vector<int>dp(n+2,-1);
-        return min(find(0,n,cost,dp),find(1,n,cost,dp));
+        vector<int>dp(n+2);
+        dp[n]=0,dp[n+1]=0; 
+        for(int i=n-1;i>=0;i--){
+            dp[i]=cost[i]+min(dp[i+1],dp[i+2]);
+        }
+        return min(dp[0],dp[1]);
     }
 };
