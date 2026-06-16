dc88eb43ce511843cef9a3d539bf5fcbfcea1cfd
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-06-16 08:42:24 +0530

    Time: 2 ms (79.92%) | Memory: 39.7 MB (69.70%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string result="";
+        for(int i=0;i<s.length();i++){
+            if('z'-s[i]<26){
+                result+=s[i];
+            }
+            else if(s[i]=='*'){
+                if(result.length()>0){
+                result.pop_back();
+                }
+            }
+            else if(s[i]=='#'){
+                result+=result;
+            }
+            else if(s[i]=='%'){
+                reverse(result.begin(),result.end());
+            }
+        }
+        return result;
+    }
+};
