5a13424a57f42607fe7e3a0705d2d57ea0d6e591
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-06-18 11:01:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (55.40%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>ans;
+        vector<int>dp(n+1,0);
         for(int i=0;i<=n;i++){
-           int count=0;
-           for(int j=0;j<32;j++){
-             if(i&(1<<j)) count++;
-           }
-           ans.push_back(count);
+            dp[i]=dp[i>>1]+(i&1);
         }
-        return ans;
+        return dp;
     }
 };
