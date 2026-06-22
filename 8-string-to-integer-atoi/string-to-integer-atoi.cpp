795f116d87474ee7d4493fcecbc540cd56483625
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-06-22 16:09:28 +0530

    Time: 3 ms (14.33%) | Memory: 12.4 MB (5.89%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,29 +1,43 @@
 class Solution {
 public:
-    int myAtoi(string s) {
-        int i=0;
-        int ans=0;
-        bool sign =true;
-        while(i<s.size()&&s[i]==' ') i++;
-        if(i==s.size()) return 0;
-
-        if(s[i]=='-'){
-            sign=false;
-            i++;
+    void helper(string s, int i, bool& sign, int& ans,bool&temp2,bool&temp3) {
+        if (i >= s.size())
+            return;
+        if (s[i] == ' '&&!temp2&&!temp3) {
+            helper(s, i + 1, sign, ans,temp2,temp3);
         }
-        else if(s[i]=='+'){
-            sign=true;
-            i++;
+        if (s[i] == '-'&&!temp2&&!temp3) {
+            sign = false;
+            temp3=true;
+            helper(s, i + 1, sign, ans,temp2,temp3);
         }
-        while(i<s.size()&&isdigit(s[i])){
-            int temp=s[i]-'0';
-            if(ans>(INT_MAX-temp)/10){
-                return sign?INT_MAX:INT_MIN;
+        if (s[i] == '+'&&!temp2&&!temp3) {
+            temp3=true;
+            sign = true;
+            helper(s, i + 1, sign, ans,temp2,temp3);
+        }
+        if (isdigit(s[i])) {
+            int temp = s[i] - '0';
+            temp2=true;
+            if (ans > (INT_MAX - temp) / 10) {
+                sign ? ans = INT_MAX : ans = INT_MIN;
+                return ;
             }
-            ans=ans*10+temp;
-            i++;
+            ans = ans * 10 + temp;
+            helper(s, i + 1, sign, ans,temp2,temp3);
+        } else {
+            return;
         }
-        if(!sign) ans=-ans;
-        return ans ;
+    }
+    int myAtoi(string s) {
+        int i = 0;
+        bool sign = true;
+        int ans = 0;
+        bool temp2=false;
+        bool temp3=false;
+        helper(s, i, sign, ans,temp2,temp3);
+        if (!sign&&ans!=INT_MIN)
+            ans = -ans;
+        return ans;
     }
 };
