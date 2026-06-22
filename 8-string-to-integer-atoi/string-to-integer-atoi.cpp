2935620477c45c87aa6a317f262a02d304c9c7c0
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-06-22 15:28:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (55.09%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,41 +1,29 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int ans = 0;
-        bool sign = true;
-        bool temp2=false;
-        int temp3=false;
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == ' '&&!temp3&&!temp2)
-                continue;
-            if(i>0&&s[i-1]=='0' &&s[i]=='-'&&ans==0) return 0;
-            if (s[i] == '-'&&!temp2&&!temp3) {
-                sign = false;
-                temp2=true;
-                continue;
-            }
+        int i=0;
+        int ans=0;
+        bool sign =true;
+        while(i<s.size()&&s[i]==' ') i++;
+        if(i==s.size()) return 0;
 
-            if (s[i] == '+' &&!temp2&&!temp3) {
-                sign = true;
-                temp2=true;
-                continue;
-            }
-
-            if (s[i] >= '0' && s[i] <= '9') {
-                int temp = s[i] - '0';
-                temp3=true;
-                if (ans > (INT_MAX - temp) / 10) {
-                    return sign ? INT_MAX : INT_MIN;
-                }
-
-                ans = ans * 10 + temp;
-            } else {
-                break;
-            }
+        if(s[i]=='-'){
+            sign=false;
+            i++;
         }
-
-        if (!sign)
-            ans = -ans;
-        return ans;
+        else if(s[i]=='+'){
+            sign=true;
+            i++;
+        }
+        while(i<s.size()&&isdigit(s[i])){
+            int temp=s[i]-'0';
+            if(ans>(INT_MAX-temp)/10){
+                return sign?INT_MAX:INT_MIN;
+            }
+            ans=ans*10+temp;
+            i++;
+        }
+        if(!sign) ans=-ans;
+        return ans ;
     }
 };
