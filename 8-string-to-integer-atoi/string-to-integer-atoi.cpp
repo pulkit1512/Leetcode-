bef973e7240b6c7ef3065ff171af6dec9da5b09e
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-06-22 15:04:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (21.81%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int ans = 0;
+        bool sign = true;
+        bool temp2=false;
+        int temp3=false;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == ' ' && ans == 0&&!temp3&&!temp2)
+                continue;
+            if(i>0&&s[i-1]=='0' &&s[i]=='-'&&ans==0) return 0;
+            if (s[i] == '-' && ans == 0&&!temp2&&!temp3) {
+                sign = false;
+                temp2=true;
+                continue;
+            }
+
+            if (s[i] == '+' && ans == 0&&!temp2&&!temp3) {
+                sign = true;
+                temp2=true;
+                continue;
+            }
+
+            if (s[i] >= '0' && s[i] <= '9') {
+                int temp = s[i] - '0';
+                temp3=true;
+                if (ans > (INT_MAX - temp) / 10) {
+                    return sign ? INT_MAX : INT_MIN;
+                }
+
+                ans = ans * 10 + temp;
+            } else {
+                break;
+            }
+        }
+
+        if (!sign)
+            ans = -ans;
+        return ans;
+    }
+};
