6651bcb633cfd2042d0fb802b2ad3fe30b708933
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-06-22 13:01:24 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (11.85%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<0) return 0;
-        if(n==0) return 0;
-        while(n!=1){
-            if(n%2==1) return 0;
-            n/=2;
-        }
-        return 1;
+        return (n>0)&&((n&(n-1))==0);
     }
 };
