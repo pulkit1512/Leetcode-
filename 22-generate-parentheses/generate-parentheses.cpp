a94e2bad940c283b9aaba1b04e8b5bc0b4483b20
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-06-23 20:16:10 +0530

    Time: 7 ms (10.42%) | Memory: 20.6 MB (7.71%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void helper(int n ,string s,int c1,int c2,vector<string>&ans){
+        if(c1==n&&c2==n){
+            ans.push_back(s);
+            return ;
+        }
+        if(c1<=n) helper(n,s+'(',c1+1,c2,ans);
+        
+        if(c2<c1) helper(n,s+')',c1,c2+1,ans);
+        
+       
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string>ans;
+        helper(n,"(",1,0,ans);
+        return ans;
+    }
+};
