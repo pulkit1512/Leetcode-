c2a8b325a4d9b30f0225b98ec2466d76ddcb61c9
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-07-14 16:49:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (77.12%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    int find(int x,int y,int m,int n,vector<vector<int>>&dp){
-        if(x>=m||y>=n) return 0;
-        if(x==m-1&&y==n-1) return 1; 
-        if(dp[x][y]!=-1) return dp[x][y];
-        return dp[x][y]=find(x+1,y,m,n,dp)+find(x,y+1,m,n,dp);
-    }
+    
     int uniquePaths(int m, int n) {
-      vector<vector<int>>dp(m+1,vector<int>(n+1,0));
-      dp[1][1]=1;
+      vector<int>dp(n+1,0);
+      dp[1]=1;
+    //   dp[1][1]=1;
       for(int i=1;i<=m;i++){
         for(int j=1;j<=n;j++){
             if(i==1&&j==1) continue;
-            dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            dp[j]=dp[j]+dp[j-1];
         }
       }
-      return dp[m][n];  
+      return dp[n];  
     }
 };
