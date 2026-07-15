9fa303694322ee4cd321969524dbf84c8b36d780
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-07-15 16:49:05 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (45.10%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
-    // int find(int m,int x,int y,vector<vector<int>>& triangle,vector<vector<int>>&dp){
-    //     if(x>=m) return 0;
-    //     if(y>=triangle[x].size()) return 0;
-    //     if(dp[x][y]!=INT_MAX) return dp[x][y];
-    //     return dp[x][y]=triangle[x][y]+min(find(m,x+1,y,triangle,dp),find(m,x+1,y+1,triangle,dp));
-    // }
     int minimumTotal(vector<vector<int>>& triangle) {
-        int m=triangle.size();
-        int n=triangle[m-1].size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,INT_MAX));
-        dp[1][1]=triangle[0][0];
-        for(int i=2;i<=m;i++){
-            for(int j=1;j<=i;j++){
-                dp[i][j]=triangle[i-1][j-1]+min(dp[i-1][j],dp[i-1][j-1]);
+        int n = triangle.size();
+        vector<vector<int>> dp(n, vector<int>(n));
+
+        for (int i = 0; i < n; i++) {
+            dp[n - 1][i] = triangle[n - 1][i];
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[i][j] = triangle[i][j] + min(dp[i + 1][j], dp[i + 1][j + 1]);
             }
         }
-        int minTotal = INT_MAX;
-        for (int j = 1; j <= m; j++) {
-            minTotal = min(minTotal, dp[m][j]);
-        }
-        
-        return minTotal;
+
+        return dp[0][0];
     }
 };
