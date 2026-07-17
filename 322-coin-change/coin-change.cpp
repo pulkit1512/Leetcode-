92026502574e104ba5dad62c413da9ab842b1563
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-07-17 08:52:59 +0530

    Time: 47 ms (34.70%) | Memory: 53.1 MB (10.50%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -8,12 +8,22 @@ public:
         else{
             return dp[index][amount]=min(1+find(index,amount-coins[index],coins,dp),find(index-1,amount,coins,dp));
         }
-    }
+    } 
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        int ans= find(n-1,amount,coins,dp);
-        if(ans==1e9) return -1;
-        return ans;
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,1e8));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                if(coins[i-1]>j) dp[i][j]=dp[i-1][j];
+                else{
+                    dp[i][j]=min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
+                }
+            }
+        }
+        if(dp[n][amount]==1e8) return -1;
+        return dp[n][amount];
     }
 };
