4d3a068a8d70e40c111a1ceb276d48d0aa394ed1
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-07-18 18:24:19 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (58.67%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,12 +1,31 @@
 class Solution {
 public:
-    int find(vector<int>&nums,int target,int index,int sum){
-        if(index==nums.size()&&sum==target) return 1;
-        if(index>=nums.size()) return 0;
-        
-        return find(nums,target,index+1,sum-nums[index])+find(nums,target,index+1,sum+nums[index]);
-    }
+    int find(vector<int>&arr,int target){
+	    int n=arr.size();
+	    vector<int>dp(target+1,0);
+	    dp[0]=1;
+	    for(int i=1;i<=n;i++){
+	        vector<int>dp2(target+1,0);
+	        dp2[0]=1;
+	        for(int j=0;j<=target;j++){
+	            if(arr[i-1]>j) dp2[j]=dp[j];
+	            else dp2[j]=dp[j]+dp[j-arr[i-1]];
+	        }
+	        dp=dp2;
+	    }
+	    return dp[target];
+	}
+	int countPartitions(vector<int>& arr, int diff) {
+		 int amount=0;
+		 for(int i=0;i<arr.size();i++){
+		     amount+=arr[i];
+		 }
+		 if((amount-diff)%2==0&&amount>=diff){
+		     return find(arr,(amount-diff)/2);
+		 }
+		 return  0;
+	}
     int findTargetSumWays(vector<int>& nums, int target) {
-        return find(nums,target,0,0);
+        return countPartitions(nums,target);
     }
 };
