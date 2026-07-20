3ad13bf7545f45297329951fb1297a0294a06272
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-07-20 08:35:52 +0530

    Time: 7 ms (86.07%) | Memory: 11.5 MB (85.51%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -3,16 +3,20 @@ public:
     int find(string word1,string word2){
         int n=word1.size();
         int m=word2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        vector<int>dp(m+1,0);
+        int prev,curr;
         for(int i=1;i<=n;i++){
+            prev=0,curr=0;
             for(int j=1;j<=m;j++){
+                prev=curr;
+                curr=dp[j];
                 if(word1[i-1]==word2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
+                    dp[j]=1+prev;
                 }
-                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                else dp[j]=max(dp[j],dp[j-1]);
             }
         }
-        return dp[n][m];
+        return dp[m];
     }
     int minDistance(string word1, string word2) {
         int lcs = find(word1,word2);
