ca1cc8a41783ea264f9f5c9cba13fab665532ef0
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-08-01 15:45:31 +0530

    Time: 386 ms (52.86%) | Memory: 211.5 MB (47.43%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -5,9 +5,10 @@ public:
         int n = prices.size();
         vector<vector<vector<int>>> dp(
             n+1, vector<vector<int>>(2, vector<int>(3, 0)));
-        for (int k = 1; k <= 2; k++) {
+        
             for (int i = n - 1; i >= 0; i--) {
                 for (int j = 0; j <= 1; j++) {
+                     for (int k = 1; k <= 2; k++){
                     int profit = INT_MIN;
                     if (j) {
                         profit = max(-prices[i] + dp[i + 1][0][k], dp[i+1][1][k]);
@@ -16,6 +17,7 @@ public:
                             max(prices[i] + dp[i + 1][1][k - 1], dp[i+1][0][k]);
                     }
                     dp[i][j][k]=profit;
+                    
                 }
                 
             }
