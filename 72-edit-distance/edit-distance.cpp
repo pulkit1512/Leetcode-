5241f341f6900a6ff661183517881c50deae7361
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-08-03 20:08:05 +0530

    Time: 9 ms (37.85%) | Memory: 10.5 MB (90.19%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-   
     int minDistance(string word1, string word2) {
-        int n=word1.size();
-        int m=word2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<=m;i++){
-            dp[0][i]=i;
-        }
-        for(int i=0;i<=n;i++){
-            dp[i][0]=i;
+        int n = word1.size();
+        int m = word2.size();
+
+        vector<int> dp1(m + 1, 0), dp2(m + 1, 0);
+
+        for (int i = 0; i <= m; i++) {
+            dp1[i] = i;
         }
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(word1[i-1]==word2[j-1]){
-                    dp[i][j]=dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j]=min({1+dp[i-1][j],1+dp[i][j-1],1+dp[i-1][j-1]});
+        for (int i = 1; i <= n; i++) {
+            dp2[0] = i;
+            for (int j = 1; j <= m; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp2[j] = dp1[j - 1];
+                } else {
+                    dp2[j] = min({1 + dp1[j], 1 + dp2[j - 1], 1 + dp1[j - 1]});
                 }
             }
+            dp1 = dp2;
         }
-        return dp[n][m];
+
+        return dp1[m];
     }
 };
