fed468bf5885fe71b36ded4286e9a9653f3385dd
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-08-04 10:14:46 +0530

    Time: 31 ms (86.70%) | Memory: 10.9 MB (81.61%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -3,30 +3,31 @@ public:
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-        
-        dp[0][0] = 1;
-        
+        vector<int>dp(m + 1, 0);
+        vector<int>dp2(m+1,0);
+        dp[0] = 1;
+        // dp2[0]=1;
         for (int j = 1; j <= m; j++) {
             if (p[j - 1] == '*') {
-                dp[0][j] = dp[0][j - 1];
+                dp[j] = dp[j - 1];
             }
         }
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 if (s[i - 1] == p[j - 1] || p[j - 1] == '?') {
-                    dp[i][j] = dp[i - 1][j - 1];
+                    dp2[j] = dp[j - 1];
                 } 
                 else if (p[j - 1] == '*') {
-                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                    dp2[j] = dp2[j - 1] || dp[j];
                 } 
                 else {
-                    dp[i][j] = false;
+                    dp2[j] = false;
                 }
             }
+            dp=dp2;
         }
         
-        return dp[n][m];
+        return dp[m];
     }
 };
