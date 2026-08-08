5878543404b9fa7e7b5f410105347767b0c1edf0
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-08-08 12:20:32 +0530

    Time: 3 ms (34.70%) | Memory: 103.3 MB (30.11%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -5,13 +5,9 @@ public:
         vector<int> adj[n];
         vector<int> outDeg(n, 0);
 
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i <edges.size() ; i++) {
             int u = edges[i][0] - 1;
             int v = edges[i][1] - 1;
-
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-
             outDeg[u]++;
             outDeg[v]++;
         }
