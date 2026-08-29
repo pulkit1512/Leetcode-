ce832eb40944907c1b32db3787105152c5a4c983
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-08-29 21:44:49 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (94.79%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        unordered_map<int,bool>m;
-        for(int i=0;i<nums.size();i++){
-            m[nums[i]]=1;
-        }
-
-        for(int i=k;i<1e8;i+=k){
-            if(!m[i]){
-                return i;
-            }
-        }
-        return -1;
+       sort(nums.begin(),nums.end());
+       int value=k;
+       for(int i=0;i<nums.size();i++){
+          if(nums[i]==value){
+            value+=k;
+          }
+       }
+       return value;
     }
 };
