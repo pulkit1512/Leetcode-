705a82d87e751a5d01a3535662a3f5f0cb19c47f
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-09-07 22:38:22 +0530

    Time: 10 ms (93.16%) | Memory: 79.8 MB (94.88%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -3,39 +3,55 @@ public:
     int totalFruit(vector<int>& fruits) {
         int first = 0, second = 0;
         int count1 = 0, count2 = 0;
-        int x = -1, y = -1; // Track the two distinct fruit types
+        int x = -1, y = -1;
+        int count = 0;
         int ans = 0;
 
         while (second < fruits.size()) {
-            int current = fruits[second];
-
-            if (current == x) {
+            // 1. Assign 1st fruit type to x
+            if (count == 0) {
+                x = fruits[second];
                 count1++;
-            } else if (current == y) {
+                count++;
+            } 
+            else if (fruits[second] == x) {
+                count1++;
+            } 
+            // 2. Assign 2nd fruit type to y
+            else if (count == 1) {
+                y = fruits[second];
                 count2++;
-            } else if (count1 == 0) { 
-                x = current;
-                count1 = 1;
-            } else if (count2 == 0) { 
-                y = current;
-                count2 = 1;
-            } else {
+                count++;
+            } 
+            else if (fruits[second] == y) {
+                count2++;
+            } 
+            // 3. A 3rd distinct fruit type has arrived
+            else {
+                count++; // count becomes 3
+            }
 
-                while (count1 > 0 && count2 > 0) {
+            // If count reaches 3, shrink window until one fruit type count drops to 0
+            if (count == 3) {
+                while (count == 3 && first <= second) {
                     if (fruits[first] == x) {
                         count1--;
-                    } else if (fruits[first] == y) {
+                        first++;
+                        if (count1 == 0) {
+                            x = fruits[second]; // Replace x with the 3rd fruit
+                            count1 = 1;
+                            count--; // count goes back to 2
+                        }
+                    } 
+                    else if (fruits[first] == y) {
                         count2--;
+                        first++;
+                        if (count2 == 0) {
+                            y = fruits[second]; // Replace y with the 3rd fruit
+                            count2 = 1;
+                            count--; // count goes back to 2
+                        }
                     }
-                    first++;
-                }
-
-                if (count1 == 0) {
-                    x = current;
-                    count1 = 1;
-                } else {
-                    y = current;
-                    count2 = 1;
                 }
             }
 
